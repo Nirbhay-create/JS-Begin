de36b80b90d3ae5596a7463d852c76f04037aa2a
max <nirbhaygorbade89@gmail.com>
2023-02-20 17:05:06 +0530

    test changes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,3 +1,10 @@
 console.log("Hello world");
 console.log("From Local Machine to Github Push via Vs Codes");
 console.log("attempt 2");
+var a ="asc"; 
+const b="sdsada";
+let n=ab(a,b);
+console.log(n);
+function ab(a,b){
+    return a+b;
+}
