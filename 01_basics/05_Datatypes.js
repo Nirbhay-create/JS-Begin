bd2264b2c3fb75c002c2349d63e09f5154c2398c
max <nirbhaygorbade89@gmail.com>
2023-02-24 21:02:19 +0530

    24-feb

---
+++ 01_basics/05_Datatypes.js
@@ -0,0 +1,11 @@
+
+// cant use alert in machine ide node it need to br in inside doc which we do in 
+//browser or with html document
+let a=10
+let b=10n;
+let c="sd"
+let d;
+let e=true
+let f=undefined;
+let g=null;
+console.table([typeof(a),typeof(b),typeof(c),typeof(d),typeof(e),typeof(f),typeof(g)]);
